c4ac7e6c80cc1827d5e1da0f4fd90e604dedbb91
qizf <qizf@outlook.com>
2023-04-10 23:16:04 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import VueCompositionAPI from '@vue/composition-api';
 
 import { BootstrapVue } from 'bootstrap-vue';
 import moment from 'moment';
-import vClickOutside from 'v-click-outside'
+import vClickOutside from 'v-click-outside';
 import i18n from '@/i18n';
 import { getLang } from '@/common/lang';
 
@@ -18,7 +18,7 @@ import '@/styles/theme-overrides/index.scss';
 
 import './initMetamask';
 
-Vue.use(vClickOutside)
+Vue.use(vClickOutside);
 Vue.use(BootstrapVue);
 
 // Vue.use(IconsPlugin);
